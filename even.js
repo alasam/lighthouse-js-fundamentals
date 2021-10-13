dd1788974ff63300d34d726fe414e46fda6bfea0
Anthony <alasam@live.com>
2021-10-12 21:33:15 -0400

    Use a function expression to find out if a number is even or odd, then output true or false

---
+++ even.js
@@ -0,0 +1,6 @@
+const isEven  = function (num) {
+  return num % 2 === 0; // checks to see if number can be divided by 2 without having remainder. If remainder is equal to 0, it is true.
+}
+
+console.log(isEven(10));  // calls isEven function expression, should assign True to variable
+console.log(isEven(11));  // calls isEven function expression, should assign False to variabl
