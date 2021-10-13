5f5b81ff33e256872ec1cc464707182671221868
Anthony <alasam@live.com>
2021-10-12 22:26:43 -0400

    Use a function expression to find if number is odd.

---
+++ odd.js
@@ -0,0 +1,5 @@
+const isOdd  = function (num) {
+  return num % 2 > 0; // checks to see if number can be divided by 2 and have a remainder greater than 0. If so, the bolean will return true.
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
