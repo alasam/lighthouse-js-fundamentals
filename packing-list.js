c14b2489743224db3028170a2d13b32930bcfa01
Anthony <alasam@live.com>
2021-10-13 04:22:04 -0400

    Added a while loop to output same result using different logic

--- packing-list.js
+++ packing-list.js
@@ -1,8 +1,18 @@
+// Array with the listed kitchen items
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
+// for loop that lists each item in the array
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 
 packingList[3];
+
+console.log("Kitchen stuff to pack:");
+// valling the variable i and using a while loop to list the array
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
