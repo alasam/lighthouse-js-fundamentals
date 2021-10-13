817893eda5a3dba34afae41dca0b1c454b981d81
Anthony <alasam@live.com>
2021-10-12 20:58:14 -0400

    function expression sayHello to say hello with names

---
+++ hello.js
@@ -0,0 +1,7 @@
+const sayHello  = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHello("Caliban"); // Will output "Hello, Caliban"
+sayHello("Miranda"); // Will output "Hello, Miranda"
+sayHello("Ferdinand"); // Will output "Hello, Ferdinand"
